12c0366fa61ab996da032426b3f68dabe3fd0b0f
Aaron Salisbury <aaron.salisbury@live.com>
2018-03-03 11:18:32 -0600

    Removed superfluous property.

--- Poiesis/Model/Application.cs
+++ Poiesis/Model/Application.cs
@@ -1,7 +1,5 @@
 ﻿using GalaSoft.MvvmLight;
 using Poiesis.Base;
-using System.Collections.Concurrent;
-using System.IO;
 
 namespace Poiesis.Model
 {
@@ -10,11 +8,6 @@ namespace Poiesis.Model
         public SelectDatabase SelectDatabase { get; set; }
         public DatabaseAttributes DatabaseAttributes { get; set; }
         public AppLogger AppLogger { get; set; }
-
-        public ConcurrentQueue<string> Events
-        {
-            get { return AppLogger.InMemorySink.Events; }
-        }
 
         public Application()
         {
